d8a62d67efca576428407b0c0b351eeec441f331
Richard Cotrina <rcotrina94@gmail.com>
2016-07-15 18:54:14 -0500

    Plantilla

---
+++ src/plantilla.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Módulos
+import pygame
+from pygame.locals import *
+import sys
+
+# Constantes
+WIDTH = 640
+HEIGHT = 480
+
+# Clases
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+# Funciones
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Pruebas Pygame")
+    while True:
+        for eventos in pygame.event.get():
+            if eventos.type == QUIT:
+                sys.exit(0)
+    return 0
+
+if __name__ == '__main__':
+    pygame.init()
+    main()
